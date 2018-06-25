3f326323c52ea85281481781e6f0b02b9be2369b
GAYATHRIalagar <36623936+GAYATHRIalagar@users.noreply.github.com>
2018-06-25 12:27:53 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s1="gayathri";
+		String s2="gayathri";
+		String s3="sameer";
+		//System.out.println(s2.compareTo(s1));
+		//System.out.println(s2.equals(s1));
+		//System.out.println(s1.concat(s3)); or System.out.println(s1+s3);
+		s1=s1.concat("manik");
+		System.out.println(s1);
+		System.out.println(s1.substring(4));
+		
+		
+	}
+}
