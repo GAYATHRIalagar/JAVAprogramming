9cd7de4090f0cd4fa3e4fcb42104fc0e9bd4058f
GAYATHRIalagar <36623936+GAYATHRIalagar@users.noreply.github.com>
2018-07-16 14:43:08 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n=5,c=0;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(" ");
+			}
+			c++;
+			for(int k=1;k<=c;k++)
+			{
+			System.out.print(i);
+			}
+			System.out.println();
+			c=c+1;
+		}
+	}
+}
+
+
+
+
+ 1
+    222
+   33333
+  4444444
+ 555555555
