1c82baa9c6d4dfd74f8a5f3d5928a63c7ebe0191
GAYATHRIalagar <36623936+GAYATHRIalagar@users.noreply.github.com>
2018-07-23 16:12:37 +0530

    Create Pattern3.java

---
+++ Pattern3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int n=5,z;
+               for(int i=1;i<=n;i++)
+               {
+       	      for(int j=i;j<n;j++)
+       	      {
+       	            System.out.print(" ");
+       	      }
+       	      for(z=1;z<=i;z++)
+       	      {
+       		System.out.print(z);
+       	      }
+       	      for(z=i-1;z>0;z--)
+       	      {
+       	      	System.out.print(z);
+       	      }
+       	      System.out.println();
+              }
+	}
+}
+
+OUTPUT:
+
+    1
+   121
+  12321
+ 1234321
+123454321
+
